ac15d369e21042be84b6918ee5066b9835d115c2
ire-rec <i.recavarren@gmail.com>
2022-09-08 16:35:12 +0200

    fixing select filter

--- src/index.js
+++ src/index.js
@@ -4,22 +4,15 @@ const moviesList = require("./data/movies.json");
 //extensiones, librerías, etc.
 const express = require("express");
 const cors = require("cors");
-<<<<<<< HEAD
 const dataBase = require("better-sqlite3");
-
-=======
 const ejs = require("ejs");
->>>>>>> 51c429e6b39080fed6045956c4960484982d91fe
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
-<<<<<<< HEAD
 const db = new dataBase("./src/movies.db", { verbose: console.log });
-=======
 server.set("view engine", "ejs");
->>>>>>> 51c429e6b39080fed6045956c4960484982d91fe
 
 // init express aplication
 const serverPort = 4000;
@@ -35,16 +28,21 @@ de una petición (request). Mirar la tabla-resumen para terminar de entenderlo*/
 //4.2 Pedir todas las películas
 server.get("/movies", (req, resp) => {
 
-  const query = db.prepare(`SELECT * FROM movies`);
-  const queryAllMovies = query.all();
-  // console.log(req.query);
-  // const selectedMovies = req.query.gender ? req.query.gender : "all";
+  // const query = db.prepare(`SELECT * FROM movies WHERE LOWER(gender)='comedia'`);
+  let userMovie = [];
+  if (req.query.gender === '') {
+    const query = db.prepare(`SELECT * FROM movies ORDER BY title DESC`); userMovie = query.all();
+  } else {
+    const query = db.prepare(`SELECT * FROM movies WHERE gender = ? ORDER BY title DESC`);
+    userMovie = query.all(req.query.gender);
+  }
+
   // const filteredMovies = moviesList.filter(
   //   (oneMovie) => selectedMovies === "all" || oneMovie.gender === selectedMovies
   // );
   const responseForUser = {
     success: true,
-    movies: queryAllMovies,
+    movies: userMovie,
   };
 
   resp.json(responseForUser);
@@ -67,10 +65,8 @@ server.post("/login", (req, resp) => {
   }
 });
 
-<<<<<<< HEAD
 //Código para crear un servidor de estáticos
-const staticServerPathWeb = "./src/public-react"; //Carp con ficheros estáticos
-=======
+
 //Motor de plantillas
 //NOTA SOBRE LAS RUTAS: en los imports, se escriben desde el index.js y en el resto, desde la raíz
 server.get("/movie/:movieId", (req, resp) => {
@@ -84,5 +80,4 @@ server.get("/movie/:movieId", (req, resp) => {
 
 //Servidor de estáticos (siempre al final, para que renderice primero lo dinámico)
 const staticServerPathWeb = "./src/public-react";
->>>>>>> 51c429e6b39080fed6045956c4960484982d91fe
 server.use(express.static(staticServerPathWeb));
